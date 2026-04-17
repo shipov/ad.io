3c29e12a7caaf782cb1c794270b0d50ff1b971df
Mikhail Shipov <mihail.korablev@gmail.com>
2026-04-17 18:02:19 +0300

    white

--- js/init.js
+++ js/init.js
@@ -26,7 +26,7 @@
 
 var myFullpage = new fullpage('#fullpage', {
 
-  sectionsColor: ['#f4f7fb', '#fff', '#7BAABE'],
+  sectionsColor: ['#fff', '#fff', '#fff'],
   anchors: ['firstPage', 'secondPage', 'thirdPage'],
     responsiveHeight: 900,
   menu: '#menu',
